82743fb6bd93b39b10506294c378abd773a2cbf8
Anthony Gedeon <anthonygedeon@Admins-MacBook-Pro.local>
2021-01-04 23:09:03 -0600

    :feature: created storyboard with design

---
+++ standup/standup/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  standup
+//
+//  Created by Anthony Gedeon on 1/4/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ standup/standup/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  standup
+//
+//  Created by Anthony Gedeon on 1/4/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
