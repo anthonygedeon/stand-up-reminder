b2b977698d53c9b52fb94879cbcf45a8aac938e5
Anthony Gedeon <anthonygedeon@Admins-MacBook-Pro.local>
2021-01-05 22:16:52 -0600

    :feature: change background color, checkbox can toggle menu

--- standup/standup/ViewController.swift
+++ standup/standup/ViewController.swift
@@ -8,19 +8,34 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    
+    
+    @IBOutlet var bottomLine: NSBox!
+    @IBOutlet var soundChoice: NSPopUpButton!
+    var toggleSound: Bool = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+//        bottomLine.shadow = NSShadow()
+//        bottomLine.layer?.shadowRadius = 3.0
+//        bottomLine.layer?.shadowOpacity = 1.0
+//        bottomLine.layer?.shadowColor = NSColor.red.cgColor
+//        bottomLine.layer?.shadowRadius = 15.0
+//        bottomLine.layer?.shadowOffset = CGSize(width: 1, height: 3)
     }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
+    
+    @IBAction func checkbox(_ sender: Any) {
+        toggleSound = !toggleSound
+        soundChoice.isHidden = toggleSound
     }
-
-
+    
+    
+    override func viewWillAppear() {
+        let color: CGFloat = 0.05
+        super.viewWillAppear()
+        view.window?.isOpaque = true
+        view.window?.backgroundColor = NSColor(red: color, green: color, blue: color, alpha: 0.8)
+    }
 }
-
